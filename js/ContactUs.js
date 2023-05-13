02341af724151ae00f34f0d1e6d7b47ff30888eb
MaubenTech <maubentech@gmail.com>
2023-05-13 17:14:54 +0100

    Benedict's Commit

---
+++ js/ContactUs.js
@@ -0,0 +1,21 @@
+// // Get the element to be hovered
+// let mapElement = document.querySelector('#map');
+
+// // Add a hover event listener to the element
+// mapElement.addEventListener('mouseover', zoomIn);
+
+// // Define the zoomIn function
+// function zoomIn() {
+//   // Increase the zoom level
+//   mapElement.style.zoom = '2';
+// }
+function zoomIn() {
+  // code for animating the zooming in of the map
+}
+
+function zoomOut() {
+  // code for animating the zooming out of the map
+}
+
+document.getElementById("map").addEventListener("mouseover", zoomIn);
+document.getElementById("map").addEventListener("mouseout", zoomOut);
