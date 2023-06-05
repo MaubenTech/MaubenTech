e55bc9fcf04bb5c3cc9b0d52a276b327ccf9564f
MaubenTech <maubentech@gmail.com>
2023-06-05 09:41:54 +0100

    Benedict's Commit

--- js/ContactUs.js
+++ js/ContactUs.js
@@ -19,3 +19,17 @@ function zoomOut() {
 
 document.getElementById("map").addEventListener("mouseover", zoomIn);
 document.getElementById("map").addEventListener("mouseout", zoomOut);
+
+
+
+
+$('.map-iframe').on("mouseenter", function(){
+  // $('.map-side').css({"display" : "block", "width" : "20%", "position" : "absolute", "left" : "-50px", "z-index" : "-50px"})
+  // $('.map-iframe').css({"transform" : "translateX(-20px) scale(1.7)", "transition" : "transform 300ms ease",})
+
+  // document.getElementById("map-iframe").src += "&z=10";
+  // console.log(document.getElementById("map-iframe").src);
+}).on("mouseleave", function(){
+  $('.map-iframe').css({"transform" : "translateX(0) scale(1.0)"}, {"transition" : "transform 300ms ease"})
+  $('.map-side').css({"display" : "none"})
+})
