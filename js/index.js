f158a2768cbb5d00590a3d052771f7cd32aec741
King Rex <anijimjamesmaurice@gmail.com>
2023-07-31 23:38:11 +0100

    Rex's Commit

--- js/index.js
+++ js/index.js
@@ -40,49 +40,158 @@ document.onscroll = () => {
 var cards = document.getElementsByClassName("card");
 var cardArray = Array.from(cards);
 
-const btn = document.getElementsByClassName("btn")[0];
-console.log(btn);
+// const btnEventListener = (event) => {
+//   event.stopPropagation();
+//   const btn = event.currentTarget;
+//   console.log("Ev:", btn);
+//   const card = btn.parentElement;
+//   console.log("Card:", card);
+//   const i = btn.children[0];
+//   console.log("i: ", i);
+//   console.log("btn clicked!");
+//   if (i.classList.contains("fa-circle-chevron-left")) {
+//     reverseClickedCard(card);
+//     console.log(document.getElementsByClassName("clicked-card")[0]);
+//     console.log("Icon has chevron-left!");
+//   }
+// };
 
-btn.addEventListener("click", (event) => {});
+const mouseover = (card) => {
+  card.children[1].children[1].children[1].style.padding = "0 15%";
+  // card.children[1].children[1].children[1].style.padding = "0 5%";
+};
+const mouseleave = (card) => {
+  card.children[1].children[1].children[1].style.padding = "0 10%";
+};
 
-cardArray.forEach((card) => {
-  const mouseover = () => {
-    card.children[1].children[1].children[1].style.padding = "0 15%";
-    // card.children[1].children[1].children[1].style.padding = "0 5%";
-  };
-  const mouseleave = () => {
-    card.children[1].children[1].children[1].style.padding = "0 10%";
-  };
+for (let i = 0; i < cardArray.length; i++) {
+  const card = cardArray[i];
   if (!card.classList.contains("clicked-card")) {
-    card.addEventListener("mouseover", mouseover);
-    card.addEventListener("mouseleave", mouseleave);
+    card.addEventListener("mouseover", mouseover(card));
+    card.addEventListener("mouseleave", mouseleave(card));
   }
 
+  // card.addEventListener("click", (event) => {
+  //   console.log("card clicked!");
+  //   const btn = card.children[0];
+
+  //   const i = card.children[0].children[0];
+
+  //   // console.log(btn);
+  //   btn.addEventListener("click", btnEventListener);
+
+  //   card.removeEventListener("mouseover", mouseover);
+  //   card.removeEventListener("mouseleave", mouseleave);
+
+  //   const form = event.currentTarget.children[2];
+
+  //   card.classList.add("clicked-card");
+
+  //   form.classList.add("active");
+
+  //   oneClickRemainingCards();
+
+  //   i.classList.remove("fa-circle-chevron-right");
+  //   i.classList.add("fa-circle-chevron-left");
+  // });
+
+  let timeout = null;
+
   card.addEventListener("click", (event) => {
-    card.removeEventListener("mouseover", mouseover);
-    card.removeEventListener("mouseleave", mouseleave);
+    if (timeout != null) {
+      clearTimeout(timeout);
+      timeout == null;
+    }
 
-    const form = event.currentTarget.children[2];
-    console.log(form);
+    const expandingCard = document.getElementById("expanding-card");
+    expandingCard.classList.add("clicked");
+    expandingCard.style.zIndex = 1000;
 
-    var clickedCard = event.currentTarget;
-    clickedCard.classList.add("clicked-card");
+    const expandingCardImage = document.getElementById("expanding-card-image");
+    expandingCardImage.src = event.currentTarget.children[1].children[0].src;
 
+    const expandingTextHeader = document.getElementById(
+      "expanding-text-header"
+    );
+    expandingTextHeader.innerHTML =
+      event.currentTarget.children[1].children[1].children[0].innerHTML;
+
+    const expandingTextContent = document.getElementById(
+      "expanding-text-content"
+    );
+
+    expandingTextContent.innerHTML =
+      event.currentTarget.children[1].children[1].children[2].innerHTML;
+
+    const expandingFormTextarea = document.getElementById(
+      "expanding-form-textarea"
+    );
+    expandingFormTextarea.value = `Hi, I'm interested in your ${expandingTextHeader.innerHTML.toLowerCase()} services.`;
+    expandingFormTextarea.placeholder = "How can we help you?";
+    expandingFormTextarea.onfocus = (event) => event.currentTarget.select();
+
+    const form = expandingCard.children[2];
     form.classList.add("active");
 
-    cardArray.forEach((card) => {
-      if (!card.classList.contains("clicked-card")) {
-        card.style.display = "none";
-      }
+    const reverseExpandBtn = expandingCard.children[0];
+    reverseExpandBtn.addEventListener("click", (event) => {
+      expandingCard.classList.remove("clicked");
+
+      timeout = setTimeout(() => {
+        if (!expandingCard.classList.contains("clicked")) {
+          expandingCard.style.zIndex = 0;
+          console.log("Timeout done!");
+        }
+      }, 600);
     });
-
-    const i = card.children[0].children[0].children[0];
-    i.classList.remove("fa-circle-chevron-right");
-    i.classList.add("fa-circle-chevron-left");
-
-    const btn = card.children[0];
-    if (card.classList.contains("clicked-card")) {
-      btn.addEventListener("click", () => {});
-    }
   });
-});
+}
+
+// function reverseClickedCard(card) {
+//   console.log("reverseClickedCard called!");
+//   reverseOneClick();
+
+//   card.addEventListener("mouseover", mouseover(card));
+//   card.addEventListener("mouseleave", mouseleave(card));
+
+//   console.log(card);
+//   card.classList.remove("clicked-card");
+
+//   const form = card.children[2];
+
+//   form.classList.remove("active");
+
+//   const i = card.children[0].children[0];
+
+//   i.classList.remove("fa-circle-chevron-left");
+//   i.classList.add("fa-circle-chevron-right");
+
+//   const btn = card.children[0];
+//   btn.removeEventListener("click", btnEventListener);
+// }
+
+// function oneClickRemainingCards() {
+//   for (let i = 0; i < cardArray.length; i++) {
+//     let card = cardArray[i];
+//     if (!card.classList.contains("clicked-card")) {
+//       card.classList.add("one-clicked");
+//       card.style.position = "absolute";
+//       card.style.left = `${i * (22 + 4)}%`;
+//     } else {
+//       if (i !== 0) {
+//         cardArray[0].style.left = `${i * (22 + 4)}%`;
+//         card.style.left = 0;
+//       }
+//     }
+//   }
+// }
+
+// function reverseOneClick() {
+//   cardArray.forEach((card) => {
+//     card.classList.remove("one-clicked");
+//     setTimeout(() => {
+//       card.style.left = null;
+//       card.style.position = "relative";
+//     }, 500);
+//   });
+// }
